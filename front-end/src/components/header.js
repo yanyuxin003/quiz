8cf4256693b5ae4d8ba213648a680da0e3e744b7
yanyuxin <617702281@qq.com>
2020-10-23 09:12:26 +0800

    [header] add header

--- front-end/src/components/header.js
+++ front-end/src/components/header.js
@@ -1,25 +1,28 @@
 import React, { Component } from 'react';
-import imgURL from '../assets/shopping.jpg';
+import {Link} from 'react-router-dom';
 import '../styles/header.css';
-import {BrowserRouter, Switch, Route, Link} from 'react-router-dom';
-import Products from "./Products";
+
 
 class Header extends Component {
   render() {
     return (
-        <BrowserRouter >
-      <div className="header">
-        <h1 className="store">商城</h1>
-        <div>
-            <img src={imgURL}></img>
-            <Link to={"/products"}>Products</Link>
+        <div className="header">
+            <div className="market">
+                <p>
+                    <Link className='toLink' to='/'>商城</Link>
+                </p>
+            </div >
+            <div className="order">
+                <p>
+                    <Link className='toLink' to='/orders'>订单</Link>
+                </p>
+            </div>
+            <div className="addProduct">
+                <p>
+                    <Link className='toLink' to='/addOrder'>添加商品</Link>
+                </p>
+            </div>
         </div>
-        {/* 父类传值给子类 */}
-        <p className="amount">{this.props.value}</p>
-        <div className="clear"></div>
-         </div>
-  <Route  path={'/products'} component={Products}></Route>
-        </BrowserRouter>
     );
   }
 }
